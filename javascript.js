8e52d2f5afa16e2b616e7135788677c47640e7fe
Janelle Tan <my.janelle.tan@gmail.com>
2025-05-05 11:10:01 +0800

    feat: added logic to get player choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    rand = Math.floor(Math.random() * 3);
+    let rand = Math.floor(Math.random() * 3);
     
     switch (rand) {
         case 0:
@@ -7,6 +7,23 @@ function getComputerChoice() {
         case 1:
             return "paper";
         case 2:
+            return "scissors";
+        default:
+            console.log("Error");
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("rock, paper, or scissors? ")
+
+    choice = choice.toLowerCase();
+
+    switch (choice) {
+        case "rock":
+            return "rock";
+        case "paper":
+            return "paper";
+        case "scissors":
             return "scissors";
         default:
             console.log("Error");
