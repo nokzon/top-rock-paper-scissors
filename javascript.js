d755095173f06ab1ee2bdaacefd1dd80bc0215dc
Janelle Tan <my.janelle.tan@gmail.com>
2025-05-13 03:34:34 +0800

    added full logic of game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    let rand = Math.floor(Math.random() * 3);
-    
+    var rand = Math.floor(Math.random() * 3)
+
     switch (rand) {
         case 0:
             return "rock";
@@ -13,84 +13,82 @@ function getComputerChoice() {
     }
 }
 
-// add event listeners
+function checkwinner(humanScore, computerScore) {
+    if (humanScore == 5) {
+        alert("You win YAY");
+        location.reload();
+    } else if (computerScore == 5) {
+        alert ("Computer wins OHNO");
+        location.reload();
+    }
+};
+
+function playGame() {
+    rock.addEventListener("click", function (e) {
+        switch (getComputerChoice()) {
+            case "rock":
+                displayResult.textContent = "Computer chooses rock. Rock and rock does... nothing! It's a tie!";
+                break;
+            case "paper":
+                computerScore++;
+                displayResult.textContent = "Computer chooses paper. Paper eats rock. You lost :(";
+                break;
+            default: // computer choose scissors
+                humanScore++;
+                displayResult.textContent = "Computer chooses scissors. Rock beats scissors. You win :)";   
+        }   
+        displayScores(); 
+        checkwinner(humanScore, computerScore)
+    });
+    paper.addEventListener("click", function (e) {
+        switch (getComputerChoice()) {
+            case "rock":
+                humanScore++;
+
+                displayResult.textContent = "Computer chooses rock. Paper eats rock. You win :)";
+                break;
+            case "paper":
+                displayResult.textContent = "Computer chooses paper. Paper and paper does... nothing. It's a tie.";
+                break;
+            default: // computer choose scissors
+                computerScore++;
+                displayResult.textContent = "Computer chooses scissors. Scissors cuts paper. You lost :(";
+        } 
+        displayScores();  
+        checkwinner(humanScore, computerScore)
+    });
+    scissors.addEventListener("click", function (e) {
+        switch (getComputerChoice()) {
+            case "rock":
+                computerScore++;
+                displayResult.textContent = "Computer chooses rock. Rock breaks scissors. You losk :(";
+                break;
+            case "paper":
+                humanScore++;
+                displayResult.textContent = "Computer chooses paper. Scissors cuts paper. You win :)";
+                break;
+            default:
+                displayResult.textContent = "Computer chooses scissors. Scissors and scissors does... nothing. It's a tie.";
+        }
+        displayScores();      
+        checkwinner(humanScore, computerScore)
+    });
+};
+
+function displayScores() {
+    displayHumanScore.textContent = `Human Score: ${humanScore}`;
+    displayComputerScore.textContent = `Computer Score: ${computerScore}`; 
+};
+
+var humanScore = 0;
+var computerScore = 0;
+
 var rock = document.querySelector("#rock");
 var paper = document.querySelector("#paper");
 var scissors = document.querySelector("#scissors");
 
-// div to display result
-var div = document.querySelector("div");
+var displayResult = document.querySelector("#displayResult");
+var displayHumanScore = document.querySelector("#humanScore");
+var displayComputerScore = document.querySelector("#computerScore");
 
-function playGame(computerChoice) {
-    humanScore = 0;
-    computerScore = 0;
-
-    rock.addEventListener("click", function (e) {
-        switch (computerChoice) {
-            case "rock":
-                div.textContent = "Computer chooses rock. Rock and rock does... nothing! It's a tie!";
-                break;
-            case "paper":
-                computerScore++;
-                div.textContent = "Computer chooses paper. Paper eats rock. You lost :(";
-                break;
-            default: // computer choose scissors
-                humanScore++;
-                div.textContent = "Computer chooses scissors. Rock beats scissors. You win :)";   
-        }    
-    });
-    paper.addEventListener("click", function (e) {
-        switch (computerChoice) {
-            case "rock":
-                humanScore++;
-                div.textContent = "Computer chooses rock. Paper eats rock. You win :)";
-                break;
-            case "paper":
-                div.textContent = "Computer chooses paper. Paper and paper does... nothing. It's a tie.";
-                break;
-            default: // computer choose scissors
-                computerScore++;
-                div.textContent = "Computer chooses scissors. Scissors cuts paper. You lost :(";
-        }  
-    });
-    scissors.addEventListener("click", function (e) {
-        switch (computerChoice) {
-            case "rock":
-                computerScore++;
-                div.textContent = "Computer chooses rock. Rock breaks scissors. You losk :(";
-                break;
-            case "paper":
-                humanScore++;
-                div.textContent = "Computer chooses paper. Scissors cuts paper. You win :)";
-                break;
-            default:
-                div.textContent = "Computer chooses scissors. Scissors and scissors does... nothing. It's a tie.";
-        }     
-    });
-}
-
-
-let totalHumanScore = 0;
-let totalComputerScore = 0;
-
-const computerSelection = getComputerChoice();
-
-playGame(computerSelection);
-
-// tally score
-if (humanScore == 1) {
-    totalHumanScore++;
-} else if (computerScore == 1) {
-    totalComputerScore++;
-}
-
-console.log(`Human score: ${totalHumanScore}, Computer score: ${totalComputerScore}`);
-
-// to determine the final winner
-if (totalHumanScore > totalComputerScore) {
-    console.log("You won the game!!");
-} else if (totalHumanScore < totalComputerScore) {
-    console.log("You lost the game...");
-} else {
-    console.log("No one wins the game. It's a tie.");
-}
+playGame();
