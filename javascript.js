0d4526b0a0e687ebe6d7502ed4b860e77f3a9bc9
Janelle Tan <my.janelle.tan@gmail.com>
2025-05-13 01:52:39 +0800

    added rock paper scissors button for user input

--- javascript.js
+++ javascript.js
@@ -13,92 +13,77 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("rock, paper, or scissors? ")
+// add event listeners
+var rock = document.querySelector("#rock");
+var paper = document.querySelector("#paper");
+var scissors = document.querySelector("#scissors");
 
-    choice = choice.toLowerCase();
+// div to display result
+var div = document.querySelector("div");
+console.log(div);
 
-    switch (choice) {
-        case "rock":
-            return "rock";
-            break;
-        case "paper":
-            return "paper";
-            break;
-        case "scissors":
-            return "scissors";
-        default:
-            console.log("Error");
-    }
-}
-
-function playGame(humanChoice, computerChoice) {
+function playGame(computerChoice) {
     humanScore = 0;
     computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == "rock") {
-            switch (computerChoice) {
-                case "rock":
-                    return "Computer chooses rock. Rock and rock does... nothing! It's a tie!";
-                    break;
-                case "paper":
-                    computerScore++;
-                    return "Computer chooses paper. Paper eats rock. You lost :(";
-                    break;
-                default: // computer choose scissors
-                    humanScore++;
-                    return "Computer chooses scissors. Rock beats scissors. You win :)";
-            }
-        } else if (humanChoice == "paper") {
-            switch (computerChoice) {
-                case "rock":
-                    humanScore++;
-                    return "Computer chooses rock. Paper eats rock. You win :)";
-                    break;
-                case "paper":
-                    return "Computer chooses paper. Paper and paper does... nothing. It's a tie.";
-                    break;
-                default: // computer choose scissors
-                    computerScore++;
-                    return "Computer chooses scissors. Scissors cuts paper. You lost :(";
-            }
-        } else { // human choose scissors
-            switch (computerChoice) {
-                case "rock":
-                    computerScore++;
-                    return "Computer chooses rock. Rock breaks scissors. You losk :(";
-                    break;
-                case "paper":
-                    humanScore++;
-                    return "Computer chooses paper. Scissors cuts paper. You win :)";
-                    break;
-                default:
-                    return "Computer chooses scissors. Scissors and scissors does... nothing. It's a tie.";
-            } 
-        }
-    }
-
-    console.log(playRound(humanChoice, computerChoice));
+    rock.addEventListener("click", function (e) {
+        switch (computerChoice) {
+            case "rock":
+                console.log("Computer chooses rock. Rock and rock does... nothing! It's a tie!");
+                break;
+            case "paper":
+                computerScore++;
+                console.log("Computer chooses paper. Paper eats rock. You lost :(");
+                break;
+            default: // computer choose scissors
+                humanScore++;
+                console.log("Computer chooses scissors. Rock beats scissors. You win :)");   
+        }    
+    });
+    paper.addEventListener("click", function (e) {
+        switch (computerChoice) {
+            case "rock":
+                humanScore++;
+                console.log("Computer chooses rock. Paper eats rock. You win :)");
+                break;
+            case "paper":
+                console.log("Computer chooses paper. Paper and paper does... nothing. It's a tie.");
+                break;
+            default: // computer choose scissors
+                computerScore++;
+                console.log("Computer chooses scissors. Scissors cuts paper. You lost :(");
+        }  
+    });
+    scissors.addEventListener("click", function (e) {
+        switch (computerChoice) {
+            case "rock":
+                computerScore++;
+                console.log("Computer chooses rock. Rock breaks scissors. You losk :(");
+                break;
+            case "paper":
+                humanScore++;
+                console.log("Computer chooses paper. Scissors cuts paper. You win :)");
+                break;
+            default:
+                console.log("Computer chooses scissors. Scissors and scissors does... nothing. It's a tie.");
+        }     
+    });
 }
+
 
 let totalHumanScore = 0;
 let totalComputerScore = 0;
 
-function tallyScore (humanScore, computerScore) {
-    if (humanScore == 1) {
-        totalHumanScore++;
-    } else if (computerScore == 1) {
-        totalComputerScore++;
-    }
-}
-
-const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playGame(humanSelection, computerSelection);
+playGame(computerSelection);
 
-tallyScore(humanScore, computerScore);
+// tally score
+if (humanScore == 1) {
+    totalHumanScore++;
+} else if (computerScore == 1) {
+    totalComputerScore++;
+}
 
 console.log(`Human score: ${totalHumanScore}, Computer score: ${totalComputerScore}`);
 
